8ada7001c1811dd402effb845bac0346bde32ce0
rawatSiddCentric <siddharth.rawat@centricconsulting.com>
2025-09-18 14:55:32 +0530

    Update OrangeHrmPage.ts

--- support/src/pages/OrangeHrmPage.ts
+++ support/src/pages/OrangeHrmPage.ts
@@ -6,6 +6,8 @@ export class OrangeHrmPage {
     private usernameSel = 'input[name="username"]';
     private passwordSel = 'input[name="password"]';
     private submitSel = 'button[type="submit"]';
+    private errorMessageSel = '//p[text()="Invalid credentials"]';
+    private blankErrorMessageSel = '//span[text()="Required"]';
 
     constructor(page: Page) {
         this.page = page;
@@ -15,13 +17,21 @@ export class OrangeHrmPage {
         await this.page.goto(process.env.BASE_URL!, { waitUntil: 'domcontentloaded' });
     }
 
-    async login(username: string, password: string) {
+    async enterCredentials(username: string, password: string) {
         await this.page.fill(this.usernameSel, username);
         await this.page.fill(this.passwordSel, password);
-        await Promise.all([
-            this.page.waitForNavigation({ waitUntil: 'networkidle' }),
+    }
+
+    async clickLogin() {
+         await Promise.all([
+            // this.page.waitForNavigation({ waitUntil: 'networkidle' }),
             this.page.click(this.submitSel)
         ]);
+    }
+
+    async login(username: string, password: string): Promise<void> {       
+        await this.enterCredentials(username, password);
+        await this.clickLogin();                
     }
 
     async getDashboardHeaderText(): Promise<string | null> {
@@ -29,4 +39,22 @@ export class OrangeHrmPage {
         const text = await this.page.textContent(this.dashboardSelector);
         return text?.trim() ?? null;
     }
+    getErrorMessage() {
+    return this.page.locator(this.errorMessageSel);
+  }
+  getBlankErrorMessage() {
+    return this.page.locator(this.blankErrorMessageSel);
+  }
+  async getErrorMessageText(): Promise<string> {
+    const locator = this.getErrorMessage();
+    await locator.waitFor({ state: 'visible', timeout: 5000 });
+    const text = await locator.innerText();
+    return text.trim();
+  }
+  async getErrorMessageBlankText(): Promise<string> {
+    const locator = this.getBlankErrorMessage();
+    await locator.waitFor({ state: 'visible', timeout: 5000 });
+    const text = await locator.innerText();
+    return text.trim();
+  }
 }
